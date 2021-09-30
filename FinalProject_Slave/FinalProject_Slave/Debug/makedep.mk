3e639e3585608879b02c891e15d59447a177eac8
MohamedEssam12 <87511244+MohamedEssam12@users.noreply.github.com>
2021-09-30 23:20:04 +0200

    Create makedep.mk

---
+++ FinalProject_Slave/FinalProject_Slave/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+APP\main.c
+
+MCAL\DIO.c
+
+MCAL\SPI.c
+
+HAL\LCD.c
+
+HAL\OUTPUT_Module.c
+
